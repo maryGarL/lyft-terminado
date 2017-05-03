d8ec1c97be7762f8e5c6b011ddc1e141ccc2a54b
Mariel Garcia <maryel_565@hotmail.com>
2017-05-03 12:22:18 -0500

    lyft-terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 var myNav=document.getElementById('navegador');
 var botoncito=document.getElementById("aparece");
+var boton=document.getElementById("become-driver");
 var boton1=document.getElementById("boton1");
 var items=document.getElementById("nav");
 var anchor1=document.getElementById("anchor1");
@@ -10,6 +11,34 @@ var telefono=document.getElementById("numero");
 var nombre=document.getElementById("nombre");
 var email=document.getElementById("email");
 var ciudad=document.getElementById("ciudad");
+var letras=/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/
+var mayuscula=/^[A-Z]{1}[a-z]{3,13}$/
+var numeros=/^[0-9]$/
+var telephone=/^[0-9]{9}$/
+var correo=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+ function validar (input,regex) {
+   if(input.value.trim().length == 0){
+     input.value = "";
+     input.nextElementSibling.style.display = "inline-block";
+   }
+   else if (regex.test(input.value)== true) {
+    input.nextElementSibling.style.display = "none";
+  }else {
+    input.value = "";
+    input.nextElementSibling.style.display = "inline-block";
+  }
+}
+
+ciudad.addEventListener("blur",function () {
+  validar(ciudad,letras);
+});
+telefono.addEventListener("blur",function () {
+  validar(telefono,telephone)});
+email.addEventListener("blur",function () {
+  validar(email,correo)});
+nombre.addEventListener("blur",function () {
+    validar(nombre,letras)})
 
 telefono.addEventListener("click", function(){
 nombre.style.display="inline-block";
